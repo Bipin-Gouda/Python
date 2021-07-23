75603be02ded237adc67f2fdd8c50d417d52d842
Bipin Gouda <bipingouda1199@gmail.com>
2021-07-23 21:43:49 +0530

    reverse of array

--- PROBLEMS/prblm2.py
+++ PROBLEMS/prblm2.py
@@ -1,3 +1,14 @@
-print("heloooooooo")
-print("helo again")
-print("again")
+# Reversing an array     
+
+arr=list(map(int,input("Enter a number: ").split()))
+print(arr[::-1])
+
+# OR
+
+end=len(arr)-1
+i=0
+while i<end:
+    arr[i],arr[end]=arr[end],arr[i]
+    i+=1
+    end-=1   
+print(arr)
