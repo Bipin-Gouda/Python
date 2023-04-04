ddc2f24e8993ca996939d76036bdd89bb3ee3462
Bipin Gouda <bipingouda1199@gmail.com>
2023-04-04 13:26:18 +0530

    Tree Mirror image

--- Data_Structures/Linked_List/SingleLL.py
+++ Data_Structures/Linked_List/SingleLL.py
@@ -1,8 +1,10 @@
+# Creating Node class
 class Node:
     def __init__(self,data=None,next=None):
         self.data=data
         self.next=next
-
+        
+# Creating Linked List class
 class LinkedList:
     def __init__(self):
         self.head=None
